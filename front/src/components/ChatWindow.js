b71b8e9e5069f278fcbfb895eedafbf616db8b7e
rosiaAI <egor-udon@mail.ru>
2024-05-21 02:08:42 +0300

    chats style change

--- front/src/components/ChatWindow.js
+++ front/src/components/ChatWindow.js
@@ -10,40 +10,52 @@ const ChatWindow = ({ messages, onSendMessage }) => {
     }
   };
   const handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === 'Enter' && !e.shiftKey) {
       handleSend();
     }
+  };
+  const handleInputChange = (e) => {
+    setMessage(e.target.value);
+    autoResizeTextarea(e.target);
+  };
+
+  const autoResizeTextarea = (textarea) => {
+    textarea.style.height = 'auto';
+    textarea.style.height = `${textarea.scrollHeight}px`;
   };
 
   return (
     <div className="chat-container">
-      <div className="chat-window">
-        {messages.map((msg, index) => (
-          <div key={index} className={`message ${msg.isMine ? 'my-message' : 'other-message'}`}>
-            <div className="message-text">
-              {msg.text}
+      <div className="chat-window-wrapper">
+        <div className="chat-window">
+          {messages.map((msg, index) => (
+            <div key={index} className={`message ${msg.isMine ? 'my-message' : 'other-message'}`}>
+              <div className="message-text">
+                {msg.text}
+              </div>
+              <div className="message-time">
+                {new Date().toLocaleTimeString()/*msg.time*/}
+              </div>
             </div>
-            <div className="message-time">
-              {new Date().toLocaleTimeString()/*msg.time*/}
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
       <div className="message-input-container">
-        <div className="message-input">
-          <input
-            type="text"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            onKeyPress={handleKeyPress}
-            placeholder="Введите сообщение..."
-          />
-          <button onClick={handleSend}>Отправить</button>
+        <div className="message-input-wrapper">
+          <div className="message-input">
+            <textarea
+              value={message}
+              onChange={handleInputChange}
+              onKeyPress={handleKeyPress}
+              placeholder="Введите сообщение..."
+            />
+            <svg class="svg-appendix" width="9" height="20"><defs><filter x="-50%" y="-14.7%" width="200%" height="141.2%" filterUnits="objectBoundingBox" id="composerAppendix"><feOffset dy="1" in="SourceAlpha" result="shadowOffsetOuter1"></feOffset><feGaussianBlur stdDeviation="1" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur><feColorMatrix values="0 0 0 0 0.0621962482 0 0 0 0 0.138574144 0 0 0 0 0.185037364 0 0 0 0.15 0" in="shadowBlurOuter1"></feColorMatrix></filter></defs><g fill="none" fill-rule="evenodd"><path d="M6 17H0V0c.193 2.84.876 5.767 2.05 8.782.904 2.325 2.446 4.485 4.625 6.48A1 1 0 016 17z" fill="#000" filter="url(#composerAppendix)"></path><path d="M6 17H0V0c.193 2.84.876 5.767 2.05 8.782.904 2.325 2.446 4.485 4.625 6.48A1 1 0 016 17z" fill="#FFF" class="corner"></path></g></svg>
+            <button onClick={handleSend}>Отправить</button>
+          </div>
         </div>
       </div>
     </div>
-
   );
-};
+}
 
 export default ChatWindow;
