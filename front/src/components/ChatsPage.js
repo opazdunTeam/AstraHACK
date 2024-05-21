30751e5c2ed1e90250589783e0dc4ba93ab1c989
rosiaAI <egor-udon@mail.ru>
2024-05-22 01:07:34 +0300

    add comments about data structures

--- front/src/components/ChatsPage.js
+++ front/src/components/ChatsPage.js
@@ -4,8 +4,10 @@ import ChatWindow from './ChatWindow';
 import './ChatsPage.css';
 
 const ChatsPage = () => {
+  //переменная для передачи информации по чатам
   const [chats, setChats] = useState([
-    { name: 'Тройственный союз', id: 1, messages: [] },
+    //future structure { name: str, id: int, messages: [ type: str, filename: str, text: str, isMine: bool, time: Date ] }
+    { name: 'Тройственный союз', id: 1, messages: [] }, //now structure { name: str, id: int, messages: [ text: str, isMine: bool, time: Date ] }
     { name: 'Флуд 2.0', id: 2, messages: [] },
     // Добавьте больше чатов по необходимости
   ]);
