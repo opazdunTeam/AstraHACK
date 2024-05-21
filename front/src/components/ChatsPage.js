dcc3da4621673cfd681905adda778d4b04be24c4
rosiaAI <egor-udon@mail.ru>
2024-05-22 01:22:33 +0300

    update todu comment

--- front/src/components/ChatsPage.js
+++ front/src/components/ChatsPage.js
@@ -6,7 +6,7 @@ import './ChatsPage.css';
 const ChatsPage = () => {
   //переменная для передачи информации по чатам
   const [chats, setChats] = useState([
-    //future structure { name: str, id: int, messages: [ type: str, filename: str, text: str, isMine: bool, time: Date ] }
+    //future structure { name: str, id: int, messages: [ type: str, filename: str, text: str, username: str, isMine: bool, time: Date ] } 
     { name: 'Тройственный союз', id: 1, messages: [] }, //now structure { name: str, id: int, messages: [ text: str, isMine: bool, time: Date ] }
     { name: 'Флуд 2.0', id: 2, messages: [] },
     // Добавьте больше чатов по необходимости
